93006cbca9d87e3d929efbf81119d4cd037eb074
VinhPN <vinh-nguyen123@hotmail.com>
2018-01-12 01:21:36 -0500

    Website
    Added pictures/smooth scrolling

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-  $(".fadeinto").hide(0).delay(500).fadeIn(1000);
-    $(".fadeinto2").hide(0).delay(900).fadeIn(1000);
-});
+  $(".fadeinto").hide(0).fadeIn(1000);
+    $(".fadeinto2").hide(0).delay(300).fadeIn(1000);
+    $(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
 
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+});
